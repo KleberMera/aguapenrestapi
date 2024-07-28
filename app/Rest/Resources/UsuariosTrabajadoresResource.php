3c96e031476cadfe82e13b53b39094347be79fa3
KleberMera <klebermera2016@gmail.com>
2024-07-28 13:26:50 -0500

    usuarios

--- app/Rest/Resources/UsuariosTrabajadoresResource.php
+++ app/Rest/Resources/UsuariosTrabajadoresResource.php
@@ -58,7 +58,14 @@ class UsuariosTrabajadoresResource extends RestResource
      * @param RestRequest $request
      * @return array
      */
- 
+    public function limits(\Lomkit\Rest\Http\Requests\RestRequest $request): array
+    {
+        return [
+            10,
+            25,
+            50
+        ];
+    }
 
     /**
      * The actions that should be linked
