81c08d167e4f0039bf63026b49f75e547bb70bb1
KleberMera <klebermera2016@gmail.com>
2024-07-30 12:55:54 -0500

    registros

--- app/Rest/Controllers/RegistroVehiculosController.php
+++ app/Rest/Controllers/RegistroVehiculosController.php
@@ -2,6 +2,8 @@
 
 namespace App\Rest\Controllers;
 
+use App\Models\Registro;
+use App\Models\RegistroVehiculos;
 use App\Rest\Controller as RestController;
 use App\Rest\Resources\RegistroVehiculosResource;
 
@@ -13,4 +15,14 @@ class RegistroVehiculosController extends RestController
      * @var class-string<\Lomkit\Rest\Http\Resource>
      */
     public static $resource = RegistroVehiculosResource::class;
+
+
+         //Funcion para obtner el ultimo id de registro
+         public function lastIdRegistroVehiculos(){
+            $ultimoRegistro = RegistroVehiculos::orderBy('id', 'desc')->first();
+            
+            return response()->json(        
+                ['id_registro_vehiculo' => $ultimoRegistro->id],
+            );
+        }
 }
