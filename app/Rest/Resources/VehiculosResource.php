e8884af9d1ce1b360a049d08f36053fc304c5dc0
KleberMera <klebermera2016@gmail.com>
2024-07-30 11:47:58 -0500

    vehiculos

---
+++ app/Rest/Resources/VehiculosResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Rest\Resources;
+
+use App\Models\Vehiculos;
+use App\Rest\Resource as RestResource;
+
+class VehiculosResource extends RestResource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    public static $model = Vehiculos::class;
+
+    /**
+     * The exposed fields that could be provided
+     * @param RestRequest $request
+     * @return array
+     */
+    public function fields(\Lomkit\Rest\Http\Requests\RestRequest $request): array
+    {
+        return [
+            'id'
+        ];
+    }
+
+    /**
+     * The exposed relations that could be provided
+     * @param RestRequest $request
+     * @return array
+     */
+    public function relations(\Lomkit\Rest\Http\Requests\RestRequest $request): array
+    {
+        return [
+            'id',
+            'tipo',
+            'placa',
+            'descripcion',
+        ];
+    }
+
+    /**
+     * The exposed scopes that could be provided
+     * @param RestRequest $request
+     * @return array
+     */
+    public function scopes(\Lomkit\Rest\Http\Requests\RestRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * The exposed limits that could be provided
+     * @param RestRequest $request
+     * @return array
+     */
+    public function limits(\Lomkit\Rest\Http\Requests\RestRequest $request): array
+    {
+        return [
+            10,
+            25,
+            50
+        ];
+    }
+
+    /**
+     * The actions that should be linked
+     * @param RestRequest $request
+     * @return array
+     */
+    public function actions(\Lomkit\Rest\Http\Requests\RestRequest $request): array {
+        return [];
+    }
+
+    /**
+     * The instructions that should be linked
+     * @param RestRequest $request
+     * @return array
+     */
+    public function instructions(\Lomkit\Rest\Http\Requests\RestRequest $request): array {
+        return [];
+    }
+}
