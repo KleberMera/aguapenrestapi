08328ff6241d7ab10d5fd0101d6fd404e12353b9
KleberMera <klebermera2016@gmail.com>
2024-07-29 22:14:51 -0500

    productos

--- app/Models/Productos.php
+++ app/Models/Productos.php
@@ -16,13 +16,5 @@ class Productos extends Model
         'stock_producto',
     ];
 
-    protected static function booted()
-    {
-        static::creating(function ($producto) {
-            // Asigna el código automáticamente antes de crear el producto
-            $latestProduct = self::orderBy('id', 'desc')->first();
-            $nextCode = $latestProduct ? str_pad(intval($latestProduct->codigo) + 1, 3, '0', STR_PAD_LEFT) : '001';
-            $producto->codigo = $nextCode;
-        });
-    }
+   
 }
